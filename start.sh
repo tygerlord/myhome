2be898433685f637adbcaa0b0b3e2ba2e9e08e03
fleblanc50 <fleblanc50@gmail.com>
2021-01-15 16:06:08 +0100

    add dir creation on start script

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #/bin/sh
 
+mkdir -p $HOME/domoticz/config
+
+mkdir -p $HOME/mosquitto/config
+mkdir -p $HOME/mosquitto/log
+mkdir -p $HOME/mosquitto/data
+
+mkdir -p $HOME/zigbee2mqtt/data
+
 CURRENT_UID=1000 CURRENT_GID=1000 docker-compose up -d
